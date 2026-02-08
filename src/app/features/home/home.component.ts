ddeb5237cd7719328c26e9a109450bc11012a752
chee <yeechee_yong@epam.com>
2026-02-09 02:38:19 +0800

    troubleshoot netlify env issue5

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { Auth0FeaturesComponent } from 'src/app/shared/components/auth0-features.component';
 import { HeroBannerComponent } from 'src/app/shared/components/hero-banner.component';
 import { PageLayoutComponent } from 'src/app/shared/components/page-layout.component';
-
+import {environment} from 'src/environments/environment'
 @Component({
   standalone: true,
   imports: [PageLayoutComponent, HeroBannerComponent, Auth0FeaturesComponent],
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(){
+    console.log('env at runtime: ',environment);
+    console.log('API URL: ',environment.api.serverUrl);
+  }
+}
